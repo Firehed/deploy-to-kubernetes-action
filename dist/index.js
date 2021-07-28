5b0be191d232be9f16020f6203120d0203d54a17
Eric Stern <eric@ericstern.com>
2021-07-28 12:50:35 -0700

    Update status at end, cleanup internals

--- dist/index.js
+++ dist/index.js
@@ -7625,23 +7625,24 @@ async function run() {
         await _actions_core__WEBPACK_IMPORTED_MODULE_0__.group('Check environment setup', envCheck);
         // await envCheck()
         // @ts-ignore
-        const deploymentId = await _actions_core__WEBPACK_IMPORTED_MODULE_0__.group('Set up Github deployment', pre);
+        const deploymentId = await _actions_core__WEBPACK_IMPORTED_MODULE_0__.group('Set up Github deployment', createDeployment);
         await _actions_core__WEBPACK_IMPORTED_MODULE_0__.group('Deploy', deploy);
-        await _actions_core__WEBPACK_IMPORTED_MODULE_0__.group('Wrap up', post);
+        await _actions_core__WEBPACK_IMPORTED_MODULE_0__.group('Update status', async () => post(deploymentId));
     }
     catch (error) {
+        // update to failed?
         _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(error.message);
     }
 }
 async function envCheck() {
     _actions_core__WEBPACK_IMPORTED_MODULE_0__.debug(JSON.stringify(process.env));
+    // Check that kubectl is available
     await _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec('kubectl version');
     await _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec('kubectl config get-contexts');
-    // Check that kubectl is available
     // check that it can talk to the cluster?
     // try to provide helpful messages if not in a usable state
 }
-async function pre() {
+async function createDeployment() {
     const token = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('token');
     const ok = _actions_github__WEBPACK_IMPORTED_MODULE_2__.getOctokit(token);
     const params = {
@@ -7655,14 +7656,7 @@ async function pre() {
     _actions_core__WEBPACK_IMPORTED_MODULE_0__.debug(JSON.stringify(deploy));
     // @ts-ignore
     const deploymentId = deploy.data.id;
-    const statusParams = {
-        owner: _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.repo.owner,
-        repo: _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.repo.repo,
-        deployment_id: deploymentId,
-        state: 'pending',
-    };
-    const status = await ok.rest.repos.createDeploymentStatus(statusParams);
-    _actions_core__WEBPACK_IMPORTED_MODULE_0__.debug(JSON.stringify(status));
+    updateStatus(deploymentId, 'pending');
     return deploymentId;
 }
 async function deploy() {
@@ -7683,122 +7677,22 @@ async function deploy() {
     args.push('--record=true');
     await _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec('kubectl', args);
 }
-async function post() {
+async function post(deploymentId) {
     // watch and wait?
+    updateStatus(deploymentId, 'success');
 }
-// async function build(): Promise<void> {
-//   const stages = getBaseStages()
-//   for (const stage of stages) {
-//     await buildStage(stage)
-//   }
-//   // TODO: refactor these, possibly parallelize
-//   const testStage = core.getInput('testenv-stage').trim()
-//   if (testStage === '') {
-//     core.info('testenv-stage not set; skipping build')
-//   } else {
-//     const testTagBranch = await buildStage(testStage)
-//     const testTag = await tagCommit(testTagBranch)
-//     await dockerPush(testTag)
-//     core.setOutput('testenv-tag', testTag)
-//   }
-//   const serverStage = core.getInput('server-stage').trim()
-//   const serverTagBranch = await buildStage(serverStage)
-//   const serverTag = await tagCommit(serverTagBranch)
-//   await dockerPush(serverTag)
-//   core.setOutput('commit', getFullCommitHash())
-//   core.setOutput('server-tag', serverTag)
-// }
-// async function buildStage(stage: string): Promise<string> {
-//   core.info(`Building stage ${stage}`)
-//   const repo = core.getInput('repository')
-//   const quiet = core.getInput('quiet') ? '--quiet' : ''
-//   const name = `${repo}/${stage}`
-//   const tagForRun = getTagForRun()
-//   const tagsToTry = [tagForRun, 'latest']
-//   // let cacheImage = ''
-//   for (const tag of tagsToTry) {
-//     const image = `${name}:${tag}`
-//     core.debug(`Pulling ${image}`)
-//     try {
-//       await exec.exec('docker', [
-//         'pull',
-//         quiet,
-//         image,
-//       ])
-//       // cacheImage = image
-//       // Don't pull fallback tags if pull succeeds
-//       break
-//     } catch (error) {
-//       // Initial pull failing is OK
-//       core.info(`Docker pull ${image} failed`)
-//     }
-//   }
-//   const dockerfile = core.getInput('dockerfile')
-//   core.debug(`Building ${stage}`)
-//   const targetTag = `${name}:${tagForRun}`
-//   const cacheFrom = Array.from(getAllPossibleCacheTargets())
-//     .flatMap(target => ['--cache-from', target])
-//   const result = await exec.exec('docker', [
-//     'build',
-//     // quiet,
-//     // '--build-arg', 'BUILDKIT_INLINE_CACHE="1"',
-//     // '--cache-from', cacheImage ? cacheImage : '""',
-//     ...cacheFrom,
-//     '--file', dockerfile,
-//     '--tag', targetTag,
-//     '--target', stage,
-//     '.'
-//   ])
-//   if (result > 0) {
-//     throw 'Docker build failed'
-//   }
-//   dockerPush(targetTag)
-//   if (isDefaultBranch()) {
-//     core.info("TODO: docker tag targetTag name:latest")
-//     core.info("TODO: docker push name:latest")
-//   }
-//   return targetTag
-// }
-// async function dockerPush(tag: string): Promise<void> {
-//   core.debug(`Pushing ${tag}`)
-//   const quiet = core.getInput('quiet') ? '--quiet' : ''
-//   const pushResult = await exec.exec('docker', [
-//     'push',
-//     quiet,
-//     tag,
-//   ])
-//   if (pushResult > 0) {
-//     throw 'Docker push failed'
-//   }
-// }
-// async function tagCommit(maybeTaggedImage: string): Promise<string> {
-//   const hash = getFullCommitHash()
-//   core.info(`Commit hash: ${hash}`)
-//   // Don't use a simple ":" split since registries can specify port
-//   const segments = maybeTaggedImage.split('/')
-//   const lastImageSegment = segments.pop()
-//   if (lastImageSegment!.includes(':')) {
-//     const segmentWithoutTag = lastImageSegment!.substring(0, lastImageSegment!.indexOf(':'))
-//     segments.push(`${segmentWithoutTag}:${hash}`)
-//   } else {
-//     segments.push(`${lastImageSegment}:${hash}`)
-//   }
-//   const commitTag = segments.join('/')
-//   await exec.exec('docker', [
-//     'tag',
-//     maybeTaggedImage,
-//     commitTag,
-//   ])
-//   return commitTag
-// }
-// function getAllPossibleCacheTargets(): Set<string> {
-//   const tags = [getTagForRun(), 'latest']
-//   const stages = getAllStages()
-//   const repo = core.getInput('repository')
-//   const out = stages.map(stage => `${repo}/${stage}`)
-//     .flatMap(image => tags.map(tag => `${image}:${tag}`))
-//   return new Set(out)
-// }
+async function updateStatus(deploymentId, state) {
+    const token = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('token');
+    const ok = _actions_github__WEBPACK_IMPORTED_MODULE_2__.getOctokit(token);
+    const params = {
+        owner: _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.repo.owner,
+        repo: _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.repo.repo,
+        deployment_id: deploymentId,
+        state,
+    };
+    const result = await ok.rest.repos.createDeploymentStatus(params);
+    console.debug(JSON.stringify(result));
+}
 run();
 
 })();
