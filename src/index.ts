0e548e2d1e9707e5fa0227e14a792d500028e9eb
Eric Stern <eric@ericstern.com>
2022-06-13 17:13:15 -0700

    Rework internals for state tracking, hopefully add resiliance

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,14 @@ async function run(): Promise<void> {
     // const previousDeploymentId = await core.group('Finding previous deployment', findPreviousDeployment)
     // core.info(`Previous deployment: ${previousDeploymentId}`)
     deploymentId = await core.group('Set up Github deployment', createDeployment)
-    await core.group('Deploy', async () => deploy(deploymentId!))
+    const deployInfo: DeployInfo = {
+      namespace: core.getInput('namespace'),
+      deployment: core.getInput('deployment'),
+      container: core.getInput('container'),
+      image: core.getInput('image'),
+    }
+
+    await core.group('Deploy', async () => deploy(deploymentId!, deployInfo))
     // await core.group('Update status', async () => post(deploymentId!))
   } catch (error) {
     // update to failed?
@@ -85,62 +92,119 @@ async function createDeployment(): Promise<DeploymentId> {
   return deploymentId
 }
 
-async function deploy(deploymentId: DeploymentId): Promise<void> {
+async function deploy(deploymentId: DeploymentId, deployInfo: DeployInfo): Promise<void> {
   const args = [
     'set',
     'image',
     'deployment',
+    `--namespace=${deployInfo.namespace}`,
+    deployInfo.deployment,
+    `${deployInfo.container}=${deployInfo.image}`,
+    '--record=true',
+    '--output=json', // This allows getting the new revision from the response to watch the rollout
   ]
 
-  const namespace = core.getInput('namespace')
-  if (namespace !== '') {
-    args.push(`--namespace=${namespace}`)
-  }
-
-  const deployment = core.getInput('deployment')
-  args.push(deployment)
-
-  const container = core.getInput('container')
-  const image = core.getInput('image')
-  args.push(`${container}=${image}`)
-
-  args.push('--record=true')
-  args.push('--output=json') // This allows getting the new revision from the response to watch the rollout
-
-  await createDeploymentStatus(deploymentId, 'in_progress')
   // Run the actual deployment command
   // TODO: figure out how to control output logging
   const deploymentOutput = await exec.getExecOutput('kubectl', args, { ignoreReturnCode: true })
   core.debug(JSON.stringify(deploymentOutput))
   if (deploymentOutput.exitCode > 0) {
+    // TODO: include stderr in this message.
     throw new Error('kubectl deployment command failed')
   }
 
-
-  // Parse response; wait for k8s to complete if desired
-  const deploymentStatus = JSON.parse(deploymentOutput.stdout)
-  const revision = deploymentStatus.metadata.annotations['deployment.kubernetes.io/revision']
-  core.debug(revision)
-
   const wait = core.getBooleanInput('wait')
-  const timeout = core.getInput('wait-timeout')
-
   if (wait) {
-    const rolloutStatusArgs = [
-      'rollout',
-      'status',
-    ]
-    if (namespace !== '') {
-      rolloutStatusArgs.push(`--namespace=${namespace}`)
-    }
-    rolloutStatusArgs.push(`deployment/${deployment}`)
-    rolloutStatusArgs.push(`--revision=${revision}`)
-    rolloutStatusArgs.push(`--timeout=${timeout}`)
-    await exec.exec('kubectl', rolloutStatusArgs)
-    // TODO: if nonzero, set to failed
+    await trackDeploymentProgress(deploymentId, deployInfo, deploymentOutput.stdout)
+  } else {
+    // fire-and-forget: assume the command goes through. This reduces the
+    // (billable!) runtime of the action, at the expense of GH status accuracy.
+    await createDeploymentStatus(deploymentId, 'success')
   }
 
-  await createDeploymentStatus(deploymentId, 'success')
+}
+
+/**
+ * This is a wrapper around the `kubectl rollout status` command to watch the
+ * deployment and attempt to keep the Kubernetes status in sync with Github.
+ *
+ * In an ideal world, this would be managed by some sort of webhook where K8S
+ * sends a request to GH, but I'm unaware of a reasonably straightforward way
+ * to accomplish this (it may be possible through some sort of Admission
+ * Controller, but that makes using this action WAY more complex and probably
+ * less reliable).
+ *
+ * This step will move the deployment (which should start as `pending`) to
+ * `in_progress`, then either `success` or `failed` depending on the outcome.
+ * It can be skipped entirely (and should be, if the action is called with
+ * `wait: false`).
+ */
+async function trackDeploymentProgress(
+  deploymentId: DeploymentId,
+  deployInfo: DeployInfo,
+  kubectlStdout: string,
+): Promise<void> {
+  // Immediately track into "in progress"
+  await createDeploymentStatus(deploymentId, 'in_progress')
+
+  // There's a bunch of parts around the `kubectl set image` that don't _quite_
+  // fit together nicely with `kubectl rollout history`, so this needs to patch
+  // over some ugliness:
+  //
+  // - If rollout history is run without the `revision` flag, it can start
+  // tracking a different rollout if one starts before the next finishes. While
+  // this can be avoided with GHA's `concurrency` option, there's no way to
+  // prevent this from external actors.
+  //
+  // - The revision flag's value doesn't seem to reliably come anywhere in the
+  // output of the deployment update's output. Trying to get it with `rollout
+  // history` creates a race condition, again with external actors.
+  //
+  // - If, for some reason, `set image deployment` changes nothing at all,
+  // there's nothing returned from the command at all - it simply exits 0 with
+  // no output on stdout.
+
+  let revision: number
+
+  if (kubectlStdout === '') {
+    // There was no change to the deployment. No great choice here but to grab
+    // the most recent rollout and hope we don't hit a race condition.
+    const historyResult = await exec.getExecOutput('kubectl', [
+      'rollout',
+      'history',
+      '--namespace', deployInfo.namespace,
+      'deployment', deployInfo.deployment,
+      '--output', 'json',
+    ])
+    const history = JSON.parse(historyResult.stdout)
+    revision = parseInt(history.metadata.annotations['deployment.kubernetes.io/revision'], 10)
+    core.debug(`Pulled revision from rollout history: ${revision}`)
+  } else {
+    const deploymentStatus = JSON.parse(kubectlStdout)
+  // This appears to return the OLD revision. Bump it by 1.
+    revision = parseInt(deploymentStatus.metadata.annotations['deployment.kubernetes.io/revision'], 10) + 1
+    core.debug(`Calculated revision from set image: ${revision}`)
+  }
+
+  const rolloutStatusArgs = [
+    'rollout',
+    'status',
+    '--namespace', deployInfo.namespace,
+    'deployment', deployInfo.deployment,
+    '--revision', `${revision}`,
+    '--timeout', core.getInput('wait-timeout'),
+  ]
+
+  // Runs the command in "watch" mode. This will exit success after some period
+  // of time if the deploy finishes, and will exit nonzero if it fails, times
+  // out, or has some other problem.
+  const exitCode = await exec.exec('kubectl', rolloutStatusArgs, { ignoreReturnCode: true })
+
+  if (exitCode === 0) {
+    await createDeploymentStatus(deploymentId, 'success')
+  } else {
+    await createDeploymentStatus(deploymentId, 'failure')
+  }
 }
 
 run()
